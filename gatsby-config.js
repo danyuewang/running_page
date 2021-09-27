66f87b1e5172f636de3c44e8a9922c3b16ca5c29
bwang <bwang@genowise.com>
2021-09-27 18:45:13 +0800

    update configure data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: `/`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://github.com/danyuewang/running_page',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
@@ -17,7 +17,7 @@ module.exports = {
     ],
   },
   plugins: [
-    'gatsby-transformer-json',
+    'gatsby-transformer-json',b
     {
       resolve: 'gatsby-source-filesystem',
       options: {
